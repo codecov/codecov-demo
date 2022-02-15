278febdd4612bea3dce6a137d554b776ec8e7a71
Tom Hu <tom@codecov.io>
2022-02-15 11:22:15 -0800

    step5: add Carryforward Flags

--- web/static/js/calculator.test.js
+++ web/static/js/calculator.test.js
@@ -31,4 +31,23 @@ describe('Calculator test suite', () => {
     calculator.delete();
     expect(calculator.currentOperand).toBe('1');
   })
+
+  test('test operation', () => {
+    const calculator = new calc.Calculator();
+    calculator.chooseOperation('+');
+    expect(calculator.operation).toBeUndefined();
+
+    calculator.appendNumber(1);
+    calculator.appendNumber(2);
+    calculator.chooseOperation('+');
+    expect(calculator.currentOperand).toBe('');
+    expect(calculator.operation).toBe('+');
+    expect(calculator.previousOperand).toBe('12');
+
+    calculator.appendNumber(3);
+    calculator.appendNumber(4);
+    expect(calculator.currentOperand).toBe('34');
+    expect(calculator.operation).toBe('+');
+    expect(calculator.previousOperand).toBe('12');
+  })
 })
