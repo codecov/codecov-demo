9818695cf408a9c2810e8e393540295708c49e10
Eileen Yang <eileenyang@Nitras-MacBook-Air.local>
2022-08-15 14:21:10 -0400

    step4: add frontend and test

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # codecov-demo
 This repository is meant to show the features and functionalities of Codecov. You can follow along [here](https://docs.codecov.com/docs/codecov-tutorial).
 
-# codeCov badge
-[![codecov](https://codecov.io/gh/eileenyang10/codecov-demo/branch/main/graph/badge.svg?token=YN03DB9ZVU)](https://codecov.io/gh/eileenyang10/codecov-demo)
+Codecov Badge: [![codecov](https://codecov.io/gh/eileenyang10/codecov-demo/branch/main/graph/badge.svg?token=YN03DB9ZVU)](https://codecov.io/gh/eileenyang10/codecov-demo)
---
+++ web/static/js/calculatorview.js
@@ -0,0 +1,57 @@
+import { Calculator } from './calculator.js';
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+document.addEventListener('keydown', function (event) {
+  let patternForNumbers = /[0-9]/g;
+  let patternForOperators = /[+\-*\/]/g
+  if (event.key.match(patternForNumbers)) {
+    event.preventDefault();
+    calculator.appendNumber(event.key)
+    calculator.updateDisplay()
+  }
+  if (event.key === '.') {
+    event.preventDefault();
+    calculator.appendNumber(event.key)
+    calculator.updateDisplay()
+  }
+  if (event.key.match(patternForOperators)) {
+    event.preventDefault();
+    calculator.chooseOperation(event.key)
+    calculator.updateDisplay()
+  }
+  if (event.key === 'Enter' || event.key === '=') {
+    event.preventDefault();
+    calculator.compute()
+  }
+});
