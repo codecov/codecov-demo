cd0f95b516423ff2e3a39ebd9a52c62d3d813929
Balu287 <52127756+Balu287@users.noreply.github.com>
2023-02-22 12:36:35 +0530

    Update test_calculator.py

--- api/calculator/test_calculator.py
+++ api/calculator/test_calculator.py
@@ -28,3 +28,6 @@ def test_divide():
     assert Calculator.divide(1.0, 2.0) == 0.5
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
+    
+def test_divide_by_0():
+    assert Calculator.divide(2.0, 0) == 'Cannot divide by 0'
