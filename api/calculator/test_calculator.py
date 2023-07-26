afd7a9b6cd8d05071e2cc2b02e0c8acf28d83f4f
joseph-sentry <joseph.sawaya@sentry.io>
2023-07-26 11:48:28 -0400

    wip

--- api/calculator/test_calculator.py
+++ api/calculator/test_calculator.py
@@ -10,11 +10,11 @@ def test_add():
 
 def test_subtract():
     assert Calculator.subtract(1, 2) == -1.0
-    # assert Calculator.subtract(2, 1) == 1.0
-    # assert Calculator.subtract(1.0, 2.0) == -1.0
-    # assert Calculator.subtract(0, 2.0) == -2.0
-    # assert Calculator.subtract(2.0, 0.0) == 2.0
-    # assert Calculator.subtract(-4, 2.0) == -6.0
+    assert Calculator.subtract(2, 1) == 1.0
+    assert Calculator.subtract(1.0, 2.0) == -1.0
+    assert Calculator.subtract(0, 2.0) == -2.0
+    assert Calculator.subtract(2.0, 0.0) == 2.0
+    assert Calculator.subtract(-4, 2.0) == -6.0
 
 def test_multiply():
     assert Calculator.multiply(1, 2) == 2.0
@@ -25,6 +25,6 @@ def test_multiply():
 
 def test_divide():
     assert Calculator.divide(1, 2) == 0.5
-    assert Calculator.divide(1.0, 2.0) == 0.5
-    assert Calculator.divide(0, 2.0) == 0
-    assert Calculator.divide(-4, 2.0) == -2.0
+    # assert Calculator.divide(1.0, 2.0) == 0.5
+    # assert Calculator.divide(0, 2.0) == 0
+    # assert Calculator.divide(-4, 2.0) == -2.0
