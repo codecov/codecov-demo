0a72b58056d1f0451c1ea485265ffd3b985824ce
joseph-sentry <joseph.sawaya@sentry.io>
2023-09-22 09:57:15 -0400

    hello world

--- api/something/something.py
+++ api/something/something.py
@@ -1,9 +1,6 @@
 class Something:
     def do_something(x):
-        if x < 0 and x > -20:
-            return x + 1
-        else:
-            return x
+        return x + 1 if x > 0 else x 
 
     def do_something_else(x, y):
         return x * 2 - y
