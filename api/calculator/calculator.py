cbc174b5bbb78156cbc44379e27573f7cd973bd3
songlf <15998233537@163.com>
2024-02-29 19:55:10 +0800

    calculator 1

--- api/calculator/calculator.py
+++ api/calculator/calculator.py
@@ -3,3 +3,13 @@ class Calculator:
         print(x)
         print(y)
         return x + y
+    def subtract(x, y):
+        return x - y
+
+    def multiply(x, y):
+        return x * y
+
+    def divide(x, y):
+        if y == 0:
+            return 'Cannot divide by 0'
+        return x * 1.0 / y
