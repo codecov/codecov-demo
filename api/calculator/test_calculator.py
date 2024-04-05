59fa497e2bee846d44e62183189c8c052e6ea843
Angelodaniel <angelo_de_voer@hotmail.com>
2024-04-05 14:07:08 +0200

    step3: change back to normal test again

--- api/calculator/test_calculator.py
+++ api/calculator/test_calculator.py
@@ -25,7 +25,7 @@ def test_multiply():
 
 def test_divide():
     assert Calculator.divide(1, 2) == 0.5
-    assert Calculator.divide(1.0, 2.0) == 0.6
+    assert Calculator.divide(1.0, 2.0) == 0.5
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
 
