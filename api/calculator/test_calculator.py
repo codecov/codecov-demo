3f576c5cc8a030ee191d3f0ba2c9bd02089efa55
Chris Stavitsky <12092849+cstavitsky@users.noreply.github.com>
2024-04-16 15:09:57 -0700

    cover divide by 0 case

--- api/calculator/test_calculator.py
+++ api/calculator/test_calculator.py
@@ -28,3 +28,4 @@ def test_divide():
     assert Calculator.divide(1.0, 2.0) == 0.5
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
+    assert Calculator.divide(10.0,0) == 'cannot divide by 0'
