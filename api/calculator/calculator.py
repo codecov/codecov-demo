af6eecd6e4e923680261d2f041ad1a8e60ba11ee
krishna.atkuru@gmail.com <krishna@take3.in>
2025-01-11 15:43:02 +0800

    step1: add calculator backend and tests

---
+++ api/calculator/calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def add(a, b):
+        return a + b
+    
+    def subtract(a, b):
+        return a - b
+    
+    def multiply(a, b):
+        return a * b
+    
+    def divide(a, b):
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
+    
+
