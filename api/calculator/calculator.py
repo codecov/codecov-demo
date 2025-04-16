21895bb046bd4819d6630ba1e6a4180da8ee3423
Joe Meissler <joe.meissler@gmail.com>
2025-04-15 20:42:37 -0700

    test

--- api/calculator/calculator.py
+++ api/calculator/calculator.py
@@ -12,3 +12,8 @@ class Calculator:
         if y == 0:
             return "Cannot divide by 0"
         return x * 1.0 / y
+
+    def untested(x, y):
+        if y == 0:
+            return "Cannot divide by 0"
+        return x * 1.0 / y
